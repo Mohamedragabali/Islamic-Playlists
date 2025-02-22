fcfd0a1c6b792812e2ff58deb850d1173689e1a5
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-02-22 22:41:03 +0200

    feat:add bottom navigation to scaffold

--- app/src/main/java/com/muslim/islamicplaylists/IslamicPlaylistsApp.kt
+++ app/src/main/java/com/muslim/islamicplaylists/IslamicPlaylistsApp.kt
@@ -4,15 +4,26 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import com.muslim.islamicplaylists.composable.CustomNavigationBar
 import com.muslim.islamicplaylists.composable.Greeting
 import com.muslim.islamicplaylists.ui.theme.IslamicPlaylistsTheme
 
 @Composable
 fun IslamicPlaylistsApp(){
+    var state by rememberSaveable { mutableStateOf("Home") }
     IslamicPlaylistsTheme {
         Scaffold(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.fillMaxSize(),
+            bottomBar = {
+                CustomNavigationBar(state) { selectedItem->
+                    state = selectedItem
+                }
+            }
         ) { innerPadding ->
             Greeting(
                 name = "Android",
@@ -21,3 +32,4 @@ fun IslamicPlaylistsApp(){
         }
     }
 }
+
