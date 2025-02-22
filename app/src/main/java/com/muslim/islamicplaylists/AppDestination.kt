7fac91da65d742b5a90a695369d1e50d6bb584c9
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-02-22 22:54:40 +0200

    feat:create AppDestination singleton class has screens names

---
+++ app/src/main/java/com/muslim/islamicplaylists/AppDestination.kt
@@ -0,0 +1,9 @@
+package com.muslim.islamicplaylists
+
+object AppDestination {
+    const val HOME = "HomeScreen"
+    const val EVERY_NEW = "EveryNewScreen"
+    const val FAVORITE = "FavoriteScreen"
+    const val DOWNLOAD  = "DownloadScreen"
+    const val SETTING = "SettingScreen"
+}
