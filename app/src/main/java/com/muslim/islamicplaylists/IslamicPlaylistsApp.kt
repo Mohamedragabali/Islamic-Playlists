253076267a003805b2bc926b8a0d450b0bfe1c60
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-02-23 02:06:36 +0200

    feat:add graph IslamicPlaylistsNavGraph composable fun to scaffold app

--- app/src/main/java/com/muslim/islamicplaylists/IslamicPlaylistsApp.kt
+++ app/src/main/java/com/muslim/islamicplaylists/IslamicPlaylistsApp.kt
@@ -9,25 +9,32 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.rememberNavController
 import com.muslim.islamicplaylists.composable.CustomNavigationBar
-import com.muslim.islamicplaylists.composable.Greeting
+import com.muslim.islamicplaylists.graphs.IslamicPlaylistsNavGraph
 import com.muslim.islamicplaylists.ui.theme.IslamicPlaylistsTheme
 import com.muslim.islamicplaylists.utils.AppDestination
 
 @Composable
 fun IslamicPlaylistsApp(){
     var state by rememberSaveable { mutableStateOf(AppDestination.HOME) }
+    val navController = rememberNavController()
     IslamicPlaylistsTheme {
         Scaffold(
             modifier = Modifier.fillMaxSize(),
             bottomBar = {
                 CustomNavigationBar(state) { selectedItem->
                     state = selectedItem
+                    navController.navigate(state) {
+                        popUpTo(navController.graph.findStartDestination().id)
+                        launchSingleTop = true
+                    }
                 }
             }
         ) { innerPadding ->
-            Greeting(
-                name = "Android",
+            IslamicPlaylistsNavGraph(
+                navController = navController,
                 modifier = Modifier.padding(innerPadding)
             )
         }
