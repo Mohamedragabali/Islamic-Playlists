94ec5176448a596b58a7396f61d90d495595278b
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-03-01 14:08:03 +0200

    refactor:replace apiService to dataSource

--- app/src/main/java/com/muslim/islamicplaylists/di/RepositoryModule.kt
+++ app/src/main/java/com/muslim/islamicplaylists/di/RepositoryModule.kt
@@ -1,23 +1,21 @@
 package com.muslim.islamicplaylists.di
 
-import com.muslim.islamicplaylists.data.network.apiservice.VideosApiService
+import com.muslim.islamicplaylists.data.datasource.VideosDataSource
 import com.muslim.islamicplaylists.data.repository.VideosRepository
 import com.muslim.islamicplaylists.data.repository.VideosRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(ViewModelComponent::class)
 object RepositoryModule {
 
-    @Singleton
     @Provides
     fun provideVideosRepository(
-        apiService: VideosApiService
+        dataSource: VideosDataSource
     ):VideosRepository{
-        return VideosRepositoryImpl( apiService)
+        return VideosRepositoryImpl( dataSource)
     }
 }
