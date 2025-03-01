c7b47aa945a6405928872e392159c6cef040af6d
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-03-01 14:06:38 +0200

    refactor:make VideosRepositoryImpl depend on layer VideosDataSource

--- app/src/main/java/com/muslim/islamicplaylists/data/network/apiservice/VideosApiService.kt
+++ app/src/main/java/com/muslim/islamicplaylists/data/network/apiservice/VideosApiService.kt
@@ -3,7 +3,6 @@ package com.muslim.islamicplaylists.data.network.apiservice
 import com.muslim.islamicplaylists.data.network.dtos.PlaylistDto
 import retrofit2.http.GET
 
-// todo use layer as abstraction between retrofit and repo 
 interface VideosApiService {
     @GET("MahmoudMabrok/MyDataCenter/main/ramadan.json")
     suspend fun getAllVideos(): PlaylistDto
--- app/src/main/java/com/muslim/islamicplaylists/data/repository/VideosRepositoryImpl.kt
+++ app/src/main/java/com/muslim/islamicplaylists/data/repository/VideosRepositoryImpl.kt
@@ -1,14 +1,14 @@
 package com.muslim.islamicplaylists.data.repository
 
-import com.muslim.islamicplaylists.data.network.apiservice.VideosApiService
+import com.muslim.islamicplaylists.data.datasource.VideosDataSource
 import com.muslim.islamicplaylists.data.network.dtos.PlaylistDto
 import javax.inject.Inject
 
 class VideosRepositoryImpl @Inject constructor(
-    private val apiService: VideosApiService
-) :VideosRepository{
-    override suspend fun getAllVideos(): PlaylistDto {
-        return apiService.getAllVideos()
+    private val videosDataSource: VideosDataSource
+) : VideosRepository {
+     override suspend fun getAllVideos(): PlaylistDto {
+        return videosDataSource.getAllVideos()
     }
 
 }
